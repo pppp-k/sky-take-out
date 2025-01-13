b84024c9e1fc5b2a6ecec3d91d6a04ba662dd870
335 <1035649196@qq.com>
2025-01-13 23:11:45 +0800

    Day 7

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -23,6 +25,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -34,6 +38,11 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //清除缓存数据
+        String key = "dish_" + dishDTO.getCategoryId();
+        //直接删除所有菜品缓存数据
+        cleanCache(key);
         return Result.success();
     }
 
@@ -61,6 +70,10 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {
         log.info("菜品批量删除：{}",ids);
         dishService.deleteBatch(ids);
+
+        //直接删除所有菜品缓存数据
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -86,6 +99,10 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //直接删除所有菜品缓存数据
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -100,6 +117,10 @@ public class DishController {
     public Result startOrStop(@PathVariable Integer status,Long id) {
         log.info("起售或停售菜品:{},{}", status, id);
         dishService.startOrStop(status,id);
+
+        //直接删除所有菜品缓存数据
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -114,6 +135,15 @@ public class DishController {
         log.info("根据分类id查询菜品:{}", categoryId);
         List<Dish> list = dishService.list(categoryId);
         return Result.success(list);
+    }
+
+    /**
+     * 清理缓存数据
+     * @param pattern
+     */
+    private void cleanCache(String pattern) {
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 
 }
---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,48 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    /**
+     * 查询购物车数据
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 更新购物车数量
+     * @param shoppingCart
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) values " +
+            "(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 根据用户id删除购物车数据
+     * @param userId
+     */
+    @Delete("delete from shopping_cart where user_id = #{usreId}")
+    void deleteByUserId(Long userId);
+
+    /**
+     * 根据id删除购物车数据
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
+}
