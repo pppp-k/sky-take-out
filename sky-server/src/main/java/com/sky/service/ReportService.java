95aea2bb0c209129385d8dcb8c99f3d3e06a0090
PP <1035649196@qq.com>
2025-01-18 22:56:30 +0800

    Day 12

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -30,4 +31,10 @@ public interface ReportService {
 
     //统计指定时间区间内的销量排名前10
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据表
+     * @param response
+     */
+    void exportBusinessData(HttpServletResponse response);
 }
